6240b6dc61be7d36b52f58411278f945bf94f0a6
CalderonCodes <cgcalderondev@gmail.com>
2023-10-31 00:13:19 -0600

    :sparkles: Add form validations

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button(props) {
   return (
-    <div className="btn lg:h-3 line rounded-lg leading-7 hover:bg-[#30ba7f] bg-Green border-none w-full lg:w-11/12 text-base lg:text-base lg:px-10 px-15 text text-neutral-50 font-Poppins font-medium
+    <div onClick={props.onClick} className="btn lg:h-3 line rounded-lg leading-7 hover:bg-[#30ba7f] bg-Green border-none w-full lg:w-11/12 text-base lg:text-base lg:px-10 px-15 text text-neutral-50 font-Poppins font-medium
     shadow-[0px_10px_0px_0px_rgba(48,186,127)] lg:shadow-[0px_5px_0px_0px_rgba(48,186,127)] hover:shadow-none">
         Claim your free trial
     </div>
--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,16 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Button from '../Buttons/Button'
 
 function Form() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  
+  const handleButtonClick = () => {
+    const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    setFirstNameError(!firstName);
+    setLastNameError(!lastName);
+    setEmailError(!email);
+    setPasswordError(!password);
+    if (!emailPattern.test(email)) {
+      setEmailError(true);
+      return;
+    }
+
+  };
+
   return (
     <div className=" w-11/12 h-full flex flex-col items-center rounded-lg line leading-7  bg-neutral-50 border-none  px-5 text normal-case font-Poppins font-normal
     shadow-[0px_10px_0px_0px_rgba(0,_0,_0,_0.2)] py-8 gap-8 lg:gap-3 ">
-        <input type="text" placeholder="First Name" className="input input-bordered w-full lg:w-11/12 h-16 font-semibold text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50" />
-        <input type="text" placeholder="Last Name" className="input input-bordered w-full lg:w-11/12 h-16 font-semibold text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50" />
-        <input type="text" placeholder="Email Address" className="input input-bordered w-full lg:w-11/12 h-16 font-semibold text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50" />
-        <input type="text" placeholder="Password" className="input input-bordered w-full lg:w-11/12 h-16 font-semibold text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50" />
-        <Button/>
-        <p className='px-5 text-center text-xs text-GrayishBlue font-medium'>
+      <div className='w-full lg:w-11/12 flex flex-col items-end gap-1 '> 
+      <input type="text" placeholder="First Name" className={`input input-bordered w-full h-16 font-semibold
+         text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50 lg:px-10 ${firstNameError ? 'border-Red placeholder:text-Red' : ''}`}
+         value={firstName}
+         onChange={(e) => {setFirstName(e.target.value); setFirstNameError(false); }}/>
+         {firstNameError && <p className="text-Red text-xs font-Poppins">First Name cannot be empty</p>}
+      </div>
+
+      <div className='w-full lg:w-11/12 flex flex-col items-end gap-1 ' >
+        <input type="text" placeholder="Last Name" className={`input input-bordered w-full h-16 font-semibold
+         text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50 lg:px-10 ${lastNameError ? 'border-Red placeholder:text-Red' : ''}`} value={lastName}
+         onChange={(e) => {setLastName(e.target.value); setLastNameError(false); }}/>
+          {lastNameError && <p className="text-Red text-xs font-Poppins">Last Name cannot be empty</p>}
+      </div>
+        
+      <div className='w-full lg:w-11/12 flex flex-col items-end gap-1 ' >
+        <input type="text" placeholder={`${emailError ? 'email@example/com' : 'Email'}`} className={`input input-bordered w-full  h-16 font-semibold
+         text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50 lg:px-10 ${emailError ? 'border-Red placeholder:text-Red' : ''}`} value={email} 
+         onChange={(e) => {setEmail(e.target.value); setEmailError(false); }}/>
+          {emailError && <p className="text-Red text-xs font-Poppins">Looks like this is not an email</p>}
+      </div>
+       <div className='w-full lg:w-11/12 flex flex-col items-end gap-1 ' >
+       <input type="text" placeholder="Password" className={`input input-bordered w-full  h-16 font-semibold
+         text-DarkBlue placeholder:text-DarkBlue placeholder:opacity-70 bg-neutral-50 lg:px-10 ${passwordError ? 'border-Red placeholder:text-Red' : ''} `} value={password}
+         onChange={(e) => {setPassword(e.target.value); setPasswordError(false); }} />
+          {passwordError && <p className="text-Red text-xs font-Poppins">Password cannot be empty</p>}
+       </div>
+        
+       
+        <Button onClick={handleButtonClick}/>
+        <p className='px-5 text-center text-xs text-GrayishBlue font-medium' >
         By clicking the button, you are agreeing to our 
         <span className="text-Red font-bold"> Terms and Services</span>
         </p>
