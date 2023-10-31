7fde8837368a1a547fca2881ae3d00854cddbfdd
CalderonCodes <cgcalderondev@gmail.com>
2023-10-30 19:46:34 -0600

    :sparkles: Add mobile trial card

---
+++ src/components/Cards/TrialCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TrialCard() {
+  return (
+    <div className="btn no-animation btn-lg line leading-7 hover:bg-Blue bg-Blue border-none w-11/12 h-24 px-20 text normal-case text-neutral-50 font-Poppins font-normal
+    shadow-[0px_10px_0px_0px_rgba(0,_0,_0,_0.2)] ">
+        Try it free 7 days then $20/mo. thereafter
+    </div>
+  )
+}
+
+export default TrialCard
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
+import TrialCard from '../../components/Cards/TrialCard';
 
 function Home() {
     const [isMobile, setIsMobile] = useState(false);
@@ -18,12 +19,19 @@ function Home() {
       }, []);
 
   return (
-    <div className='min-h-screen bg-Red' 
+    <div className='min-h-screen bg-Red flex flex-col items-center' 
     style={{ backgroundImage: isMobile ?
     'url(/assets/bg-intro-mobile.png)' : 'url(/assets/bg-intro-desktop.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
         
-        <h1> </h1>
-    
+        <h1 className='text-neutral-50 text-3xl font-semibold font-Poppins p-24 pb-10 text-center'> Learn to code by watching others</h1>
+        <p className='text-neutral-50 text-xl font-normal font-Poppins px-10 text-center'>
+        See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+        but understanding how developers think is invaluable. 
+        </p>
+        <div className='w-full flex  flex-col items-center py-20'>
+            <TrialCard/>
+            
+        </div>
     </div>
   )
 }
